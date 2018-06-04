edb6e2d2bfe8c1584d10a9a997b335235f679d33
Mark-Hynes <mrkhynes1@gmail.com>
2018-06-04 15:34:55 +0100

    Add project files.

---
+++ EthnoBot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EthnoBot.Startup))]
+namespace EthnoBot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
